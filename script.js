d2291be06cf0c6bd2f808e940c8bca21c0942354
J.Nicewander <jnicewander@live.com>
2020-05-29 13:46:24 -0400

    Added an object literal to track number of coins.

--- script.js
+++ script.js
@@ -1,16 +1,28 @@
+const coins = {
+    penny: 0,
+    nickel: 0,
+    dime: 0,
+    quarter: 0,
+    incrementCoins(number, coin) {
+        coins[coin] += parseInt(number);
+        console.log(coins);
+    }
+}
+
 const number = () => document.getElementById('number').value;
 const coin = () => document.getElementById('coin').value;
 
-function displayCoins(number, coins) {
+function displayCoins(number, coin) {
     const coinDisplay = document.getElementById('coinDisplay');
     for (let i = 0; i < number; i++) {
-        coinDisplay.innerHTML += `<div class='coin ${coins}'>${coins}</div>`;
+        coinDisplay.innerHTML += `<div class='coin ${coin}'>${coin}</div>`;
     };
 };
 
 document.getElementById('form').addEventListener('submit', event => {
     event.preventDefault();
     displayCoins(number(), coin());
+    coins.incrementCoins(number(), coin());
 });
 
 document.getElementById('coinDisplay').addEventListener('click', event => {
@@ -18,4 +30,3 @@ document.getElementById('coinDisplay').addEventListener('click', event => {
         event.target.parentNode.removeChild(event.target);
     }
 });
-
