8014c44283e3b881887250b2be33e80bc0ab5cd0
J.Nicewander <jnicewander@live.com>
2020-05-30 16:46:16 -0400

    Changed object literal to an Array of objects. Modified relevant functions.

--- script.js
+++ script.js
@@ -1,17 +1,44 @@
-const coins = {
-    penny: 0,
-    nickel: 0,
-    dime: 0,
-    quarter: 0,
-    incrementCoins(number, coin) {
-        coins[coin] += parseInt(number);
-        console.log(coins);
+const bankAccount = [
+    {
+        name: 'penny',
+        qty: 0,
+        mutiplier: 0.01
     },
-    decrementCoins(coin) {
-        coins[coin] -= 1;
-        console.log(coins);
+    {
+        name: 'nickel',
+        qty: 0,
+        mutiplier: 0.05
+    },
+    {
+        name: 'dime',
+        qty: 0,
+        mutiplier: 0.10
+    },
+    {
+        name: 'quarter',
+        qty: 0,
+        mutiplier: 0.25
     }
+];
+
+function incrementCoins(number, coin) {
+    bankAccount.forEach(obj => {
+        if(obj.name === coin) {
+            obj.qty += parseInt(number);
+        }
+    });
+    console.log(bankAccount);
 }
+
+function decrementCoins(coin) {
+    bankAccount.forEach(obj => {
+        if(obj.name === coin) {
+            obj.qty -= 1;
+        }
+    });
+    console.log(bankAccount);
+}
+
 
 const number = () => document.getElementById('number').value;
 const coin = () => document.getElementById('coin').value;
@@ -23,31 +50,32 @@ function displayCoins(number, coin) {
     };
 };
 
-function calculateTotal() {
-    let pennies = coins.penny * 0.01;
-    let nickels = coins.nickel * 0.05;
-    let dimes = coins.dime * 0.10;
-    let quarters = coins.quarter * 0.25;
-    return pennies + nickels + dimes + quarters;
+function calculateTotal(arr) {
+    let total = 0;
+    arr.forEach(obj => {
+        total += obj.qty * obj.mutiplier
+    })
+
+    return total;
 }
 
 function displayAmount() {
     let display = document.getElementById('total');
     display.innerText = "";
-    display.append(`$ ${calculateTotal().toFixed(2)}`);
+    display.append(`$ ${calculateTotal(bankAccount).toFixed(2)}`);
 }
 
 document.getElementById('form').addEventListener('submit', event => {
     event.preventDefault();
     displayCoins(number(), coin());
-    coins.incrementCoins(number(), coin());
+    incrementCoins(number(), coin());
     displayAmount();
 });
 
 document.getElementById('coinDisplay').addEventListener('click', event => {
     if(event.target !== event.currentTarget) {
         event.target.parentNode.removeChild(event.target);
-        coins.decrementCoins(event.target.classList[1]);
+        decrementCoins(event.target.classList[1]);
     }
     displayAmount();
 });
